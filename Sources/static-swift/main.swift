109511d1dfa931a53f16b85b746a34b0c33ff1b8
Igor Makarov <igormaka@gmail.com>
2019-07-16 19:58:56 +0300

    fix linux build

--- Sources/static-swift/main.swift
+++ Sources/static-swift/main.swift
@@ -1,26 +1,13 @@
 
-import Foundation
+import PerfectLib
 
-struct FileHandlerOutputStream: TextOutputStream {
-    private let fileHandle: FileHandle
-    let encoding: String.Encoding
+try Dir("_site").create()
 
-    init(_ path: String, encoding: String.Encoding = .utf8) throws {
-        FileManager.default.createFile(atPath: path, contents: nil)
-        self.fileHandle = try FileHandle(forWritingTo: URL(fileURLWithPath: path))
-        self.encoding = encoding
-    }
-
-    mutating func write(_ string: String) {
-        if let data = string.data(using: encoding) {
-            fileHandle.write(data)
-        }
-    }
-}
-
-try FileManager.default.createDirectory(atPath: "_site", withIntermediateDirectories: true)
-var file = try FileHandlerOutputStream("_site/aaa.txt")
+let file = File("_site/aaa.txt")
+try file.open(.readWrite)
 
 for i in 0..<1000 {
-    print(i, to: &file)
+    try file.write(string: String(i) + "\n")
 }
+
+file.close()
