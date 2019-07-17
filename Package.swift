d972f7d2043fe64501efd9e5417b1e8c05f98f0b
Igor Makarov <igormaka@gmail.com>
2019-07-17 12:36:44 +0300

    tz mapper

--- Package.swift
+++ Package.swift
@@ -5,15 +5,18 @@ import PackageDescription
 
 let package = Package(
     name: "static-swift",
+    platforms: [
+        .macOS(.v10_11), .iOS(.v9), .tvOS(.v9), .watchOS(.v2)
+    ],
     dependencies: [
-		.package(url: "https://github.com/igor-makarov/Perfect", .branch("master")),
-		.package(url: "https://github.com/igor-makarov/Perfect-LinuxBridge", .branch("master")),
+		.package(url: "https://github.com/yaslab/CSV.swift", from: "2.0.0"),
+        .package(url: "https://github.com/weichsel/ZIPFoundation", from: "0.9.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "static-swift",
-            dependencies: ["PerfectLib"]),
+            dependencies: ["CSV", "ZIPFoundation"]),
     ]
 )
---
+++ Sources/static-swift/Location.swift
@@ -0,0 +1,13 @@
+//
+//  Location.swift
+//  CSV
+//
+//  Created by Igor Makarov on 17/07/2019.
+//
+
+import Foundation
+
+public struct CLLocationCoordinate2D {
+    let latitude: Double
+    let longitude: Double
+}
